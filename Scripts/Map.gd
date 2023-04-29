20d275355438309f00a754f8b26f9d1140d75003
HolyBlue <hugobarrandon@gmail.com>
2023-04-29 11:02:08 +0200

    update tiles

--- Scripts/Map.gd
+++ Scripts/Map.gd
@@ -1,6 +1,6 @@
 extends TileMap
 
-var blank : Vector2i = Vector2i(1,22)
+var blank : Vector2i = Vector2i(2,0)
 
 func _unhandled_input(event):
 	if(event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed()):
